50147fc4495e220df9866fc6987e2a9c222bb3b0
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-01 03:23:12 +0000

    formatting

--- main.cs
+++ main.cs
@@ -9,27 +9,27 @@ class Program
 {
     public static void Main(string[] args)
     {
-         // This function accepts user input
-         double lengthSquare;
-         double perimeter;
+        // This function accepts user input
+        double lengthSquare;
+        double perimeter;
          
-         // input
-         Console.WriteLine("This program finds the perimeter of a sqaure. ");
-         Console.WriteLine("");
-         Console.WriteLine("the formula is: L x 4");
-         Console.WriteLine("");
-         Console.WriteLine("please enter the measurements (cm)");
-         Console.WriteLine("");
+        // input
+        Console.WriteLine("This program finds the perimeter of a sqaure. ");
+        Console.WriteLine("");
+        Console.WriteLine("the formula is: L x 4");
+        Console.WriteLine("");
+        Console.WriteLine("please enter the measurements (cm)");
+        Console.WriteLine("");
          
-         Console.Write("Input the measurement for the length :");
+        Console.Write("Input the measurement for the length :");
         lengthSquare = Convert.ToDouble(Console.ReadLine());
          
-         // process
-         perimeter = lengthSquare * 4;
+        // process
+        perimeter = lengthSquare * 4;
         
-         // output
-         Console.WriteLine("");
-         Console.WriteLine("The perimeter of the square is, " + perimeter.ToString("0.00") + " cm");
-         Console.WriteLine("\nDone.");
+        // output
+        Console.WriteLine("");
+        Console.WriteLine("The perimeter of the square is, " + perimeter.ToString("0.00") + " cm");
+        Console.WriteLine("\nDone.");
     }
 }
